1fe03ac1037ebe1d23f710494efa11c633094e6b
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-11-09 22:55:25 -0500

    100 task correction

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 ARGV.each do|a|
-    var = a.scan(/(?<=from:)[A-Z|a-z|0-9|\+]+|(?<=to:)\+[0-9]+|(?<=flags:).{12}/)
+    var = a.scan(/(?<=from:)[A-Z|a-z|0-9|\+]+|(?<=to:)[+0-9]+|(?<=flags:)[-10:]+/)
     print var.join(',')
     puts""
 end
