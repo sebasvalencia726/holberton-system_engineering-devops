21b759e1b01553ca44757ded390e0fc52599d0af
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-12-07 16:56:13 -0500

    0 task

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#Using Puppet, create a file in /tmp.
+file { '0-create_a_file.pp':
+  path    => '/tmp/holberton',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
